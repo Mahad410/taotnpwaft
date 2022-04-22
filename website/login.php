5f23c237b04738090b49704f5dc14b5c7a5052f8
Hritik R <hritik.r7@outlook.com>
2022-04-22 12:57:22 +0530

    Add authentication module, minor changes
    Fix apply to #95
    ### Added
    - Minor CSS Changes
    - TAOTAuth.js : handles ajax request  and validation for login, register.
    - CreateUser():  handles registration of new user
    - CheckUserName(): checks if username already exists
    - CheckEmail checks if email already exists
    ### Modified
    - Register Button
    - Register Modal Redesign
    - ValidateLogin(): implemented prepared statement to prevent sql injection.
    - login.php and register.php:  to implement JSON response.
    ### Removed
    - Login() and Register(): TAOTGeneral.js

--- website/login.php
+++ website/login.php
@@ -1,7 +1,13 @@
 <?php
-  include "DatabaseHelper.php";
-  $result = ValidateLogin($_POST['UserName'], $_POST['Password']);
-  $json = GetLoginTableData("SELECT * FROM logins");
-  echo $result;
-?>
-  
+include "DatabaseHelper.php";
+$result = ValidateLogin($_POST['username'], $_POST['password']);
+$responseArr = array();
+if ($result) {
+  $responseArr['code'] = 200;
+  $responseArr['msg'] = '<span class="success">Login Successful, You will be redirected to your profile in <span class="tm-c">3</span> seconds</span>';
+} else {
+  $responseArr['code'] = 400;
+  $responseArr['msg'] = '<span class="error">Invalid username or password</span>';
+}
+
+echo json_encode($responseArr, JSON_PRETTY_PRINT);
--- website/register.php
+++ website/register.php
@@ -1,24 +1,14 @@
-<?php
-require('phpfile.php');
-// If form submitted, insert values into the database.
+<?php 
+require('DatabaseHelper.php');
 if (isset($_REQUEST['username'])){
-        // removes backslashes
-	$username = stripslashes($_REQUEST['username']);
-        //escapes special characters in a string
-	$username = mysqli_real_escape_string($con,$username); 
-	$email = stripslashes($_REQUEST['email']);
-	$email = mysqli_real_escape_string($con,$email);
-	$password = stripslashes($_REQUEST['password']);
-	$password = mysqli_real_escape_string($con,$password);
-	$trn_date = date("Y-m-d H:i:s");
-        $query = "INSERT into `users` (username, password, email, trn_date)
-VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
-        $result = mysqli_query($con,$query);
+        $result = CreateUser($_REQUEST['email'], $_REQUEST['username'], $_REQUEST['password']);
+        $responseArr = array();
         if($result){
-            echo "<div class='form'>
-<h3>You are registered successfully.</h3>
-<br/>Click here to <a href='login.php'>Login</a></div>";
-        }
+        $responseArr['code'] = 200;
+        $responseArr['msg'] = '<span class="success">You are registered successfully.</span>';
     }else{
-?>
-<?php } ?>
+        $responseArr['code'] = 400;
+        $responseArr['msg'] =  '<span class="error">Error occured while registration</span>';
+ } 
+ echo json_encode($responseArr, JSON_PRETTY_PRINT);
+}
